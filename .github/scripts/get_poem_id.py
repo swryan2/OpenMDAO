319a19161d9c24bdf1f2a6c0b39be5552a3bd31b
swryan <swryan@users.noreply.github.com>
2024-06-10 14:57:18 -0400

    repo

--- .github/scripts/get_poem_id.py
+++ .github/scripts/get_poem_id.py
@@ -30,12 +30,16 @@ def get_poem_id(repository, pull_id):
     print(f"Checking Pull Request #{pull_id} for associated issue...")
     print("-------------------------------------------------------------------------------")
     try:
-        pull_json = subprocess.check_output(["gh", "--repo", repository,
-                                             "issue", "view", "--json", "body", pull_id])
+        pull_json = subprocess.check_output(f"gh --repo {repository} issue view --json body {pull_id}",
+                                              stderr=subprocess.STDOUT, shell=True,
+                                              timeout=10, universal_newlines=True);
     except subprocess.CalledProcessError as err:
         print(f"Unable to access pull request #{pull_id}:\nrc={err.returncode}")
         print(pull_json)
         return ERROR
+
+    from pprint import pprint
+    pprint(pull_json)
 
     pull_body = json.loads(pull_json)["body"]
 
@@ -60,6 +64,7 @@ def get_poem_id(repository, pull_id):
     print("-------------------------------------------------------------------------------")
 
     try:
+        repository.replace('swryan', 'OpenMDAO')
         issue_json = subprocess.check_output(f"gh --repo {repository} issue view --json body {issue_id}",
                                               stderr=subprocess.STDOUT, shell=True,
                                               timeout=10, universal_newlines=True);
